3b4a71771c412197a77ce6d0b293d813939f89f4
MarcConroy <C00161690@itcarlow.ie>
2014-11-22 15:16:28 +0000

    Added methods and images for HUD

---
+++ hudDisplay.h
@@ -0,0 +1,22 @@
+#ifndef HUDDISPLAY_H
+#define HUDDISPLAY_H
+
+#include "Box2D\Box2D.h"
+#include "LTexture.h"
+
+class hudDisplay {
+public:
+	hudDisplay();
+	hudDisplay(b2World* world, SDL_Renderer* gRenderer, b2Vec2 position);
+	~hudDisplay();
+
+	void Draw();
+private:
+	int m_health;
+	int m_thurst;
+
+	LTexture mHealthTexture;
+	LTexture mThurstTexture;
+}
+
+#endif
