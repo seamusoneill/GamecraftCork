a43d174da994b998ec4b5206fee3475ac1521ff8
seamus <seamusoneill@live.ie>
2014-11-22 12:18:15 +0000

    Player in main

--- main.cpp
+++ main.cpp
@@ -10,8 +10,10 @@
 #include <vector>
 #include "ContactListener.h"
 #include "CONSTANTS.h"
+#include "Player.h"
 
 
+Player* p;
 
 //Program Variables
 bool isRunning = true;
@@ -28,34 +30,39 @@ SDL_Renderer* gRenderer;
 SDL_Event e;
 
 void SetupWorld() {
-	b2Vec2 gravity(0, -9.81f);
+	b2Vec2 gravity(0, 0);
 	m_world = new b2World(gravity);
 	m_world->SetContactListener(ContactListener::getListener());
 }
 
 void SetupSDL() {
-	window = SDL_CreateWindow( "Treasure Flood", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, CONSTANTS::SCREEN_WIDTH,CONSTANTS::SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-	gRenderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
-    IMG_Init( IMG_INIT_PNG );
+	window = SDL_CreateWindow("Treasure Flood", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, CONSTANTS::SCREEN_WIDTH, CONSTANTS::SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	gRenderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	IMG_Init(IMG_INIT_PNG);
 }
 
 void Initialize()
 {
 	SetupWorld();
 	SetupSDL();
+
+	p = new Player(m_world, gRenderer, b2Vec2(0, 0), 40);
 }
 
 void DrawEntities() {
 
-	SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
-	SDL_RenderClear( gRenderer );
+	SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+	SDL_RenderClear(gRenderer);
 
-	SDL_RenderPresent( gRenderer );
+	b2Vec2 offset = b2Vec2((p->GetPosition().x*METRESTOPIXELS) - CONSTANTS::SCREEN_WIDTH / 2, (p->GetPosition().y*METRESTOPIXELS) + CONSTANTS::SCREEN_HEIGHT / 2);
+
+	p->Draw(gRenderer, offset);
+	SDL_RenderPresent(gRenderer);
 }
 
 void Quit() {
-	SDL_DestroyWindow( window );
-	SDL_DestroyRenderer( gRenderer );
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(gRenderer);
 	IMG_Quit();
 	SDL_Quit();
 	isRunning = false;
@@ -78,7 +85,7 @@ void Update() {
 
 }
 
-int main( int argc, char* args[] ) {
+int main(int argc, char* args[]) {
 	Initialize();
 
 	while (isRunning) Update();
